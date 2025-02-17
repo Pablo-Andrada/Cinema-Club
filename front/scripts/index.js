12e3c61d5143e908b443328e1cda98d503fd7ac4
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2025-02-17 01:27:37 -0300

    Creando el bundler de js

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,6 +1,4 @@
-// Asegurar que el DOM se haya cargado antes de ejecutar el código
-window.onload = function () {
-    const movieContainer = document.getElementById("movie-container");
+
 
     // Función para obtener películas desde la API
     $.get("https://students-api.up.railway.app/movies", function (movies) {
@@ -10,29 +8,5 @@ window.onload = function () {
         movieContainer.innerHTML = "<p>Error al cargar las películas. Intenta nuevamente.</p>";
     });
 
-    // Función para renderizar las películas
-    function renderMovies(movies) {
-        movieContainer.innerHTML = ""; // Limpiar el contenedor antes de agregar nuevas tarjetas
+    
 
-        movies.forEach(movie => {
-            const movieCard = document.createElement("div");
-            movieCard.classList.add("movie-card");
-
-            movieCard.innerHTML = `
-                <img src="${movie.poster}" alt="${movie.title}">
-                <div class="movie-info">
-                    <h2>${movie.title}</h2>
-                    <p class="duration">${movie.duration}</p>
-                    <p class="year">${movie.year}</p>
-                    <p class="director">Director: ${movie.director}</p>
-                    <p class="rate">Rating: ${movie.rate}</p>
-                    <div class="genre">
-                        ${movie.genre.map(g => `<span>${g}</span>`).join("")}
-                    </div>
-                </div>
-            `;
-
-            movieContainer.appendChild(movieCard);
-        });
-    }
-};
---
+++ front/scripts/renderMovies.js
@@ -0,0 +1,27 @@
+const movieContainer = document.getElementById("movie-container");
+
+// Función para renderizar las películas
+function renderMovies(movies) {
+    movieContainer.innerHTML = ""; // Limpiar el contenedor antes de agregar nuevas tarjetas
+
+    movies.forEach(movie => {
+        const movieCard = document.createElement("div");
+        movieCard.classList.add("movie-card");
+
+        movieCard.innerHTML = `
+            <img src="${movie.poster}" alt="${movie.title}">
+            <div class="movie-info">
+                <h2>${movie.title}</h2>
+                <p class="duration">${movie.duration}</p>
+                <p class="year">${movie.year}</p>
+                <p class="director">Director: ${movie.director}</p>
+                <p class="rate">Rating: ${movie.rate}</p>
+                <div class="genre">
+                    ${movie.genre.map(g => `<span>${g}</span>`).join("")}
+                </div>
+            </div>
+        `;
+
+        movieContainer.appendChild(movieCard);
+    });
+}
