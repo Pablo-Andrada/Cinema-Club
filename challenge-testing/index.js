2cd7ddabc4bdb32d514266013942a5dedbf981bb
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2025-02-18 01:53:58 -0300

    Funcionando

--- challenge-testing/index.js
+++ challenge-testing/index.js
@@ -1,3 +1,23 @@
-class carritoCompra { };
-
-module.exports = carritoCompra;
+class CarritoCompra {
+    constructor() {
+      this.productos = [];
+      this.total = 0;
+    }
+  
+    agregarProducto(producto) {
+      this.productos.push(producto);
+    }
+  
+    calcularTotal() {
+      return this.total || this.productos.reduce((acc, producto) => acc + producto.precio, 0);
+    }
+  
+    aplicarDescuento(porcentaje) {
+      const totalSinDescuento = this.calcularTotal();
+      const descuento = totalSinDescuento * (porcentaje / 100);
+      this.total = totalSinDescuento - descuento;
+    }
+  }
+  
+  module.exports = CarritoCompra;
+  
