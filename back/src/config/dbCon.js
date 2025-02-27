250adc8b12bccc6ddd83824aad8f61e354b79e67
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2025-02-27 01:56:14 -0300

    Protegido

--- back/src/config/dbCon.js
+++ back/src/config/dbCon.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
+
 
 const dbCon = async () => {
     //Realiza la conexion con la base de datos
-    await mongoose.connect("mongodb+srv://pablomatiasandrada:D8FXWky6463ggsZi@prueba.oripz.mongodb.net/movies?retryWrites=true&w=majority")
+    await mongoose.connect(process.env.MONGO_URI)
+    //console.log(process.env.MONGO_URI);
+    
 }
 
 module.exports = dbCon;
